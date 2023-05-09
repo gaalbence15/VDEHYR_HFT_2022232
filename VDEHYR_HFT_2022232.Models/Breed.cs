af20ae88eb131cd96ddf79d654a67d25a8e559ef
gaalbence15 <gaalbence15@gmail.com>
2023-05-09 15:43:08 +0200

    Models add classes

---
+++ VDEHYR_HFT_2022232.Models/Breed.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VDEHYR_HFT_2022232.Models
+{
+    public class Breed
+    {
+    }
+}
--- VDEHYR_HFT_2022232.Models/Dog.cs
+++ VDEHYR_HFT_2022232.Models/Dog.cs
@@ -2,7 +2,7 @@
 
 namespace VDEHYR_HFT_2022232.Models
 {
-    public class Class1
+    public class Dog
     {
     }
 }
