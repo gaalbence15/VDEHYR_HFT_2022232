1e485e1a941fe9a258683f7ffa87b1abfc1c8ff1
gaalbence15 <gaalbence15@gmail.com>
2023-05-09 18:10:28 +0200

    Starts database connection class

---
+++ VDEHYR_HFT_2022232.Repository/Database/DogsDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VDEHYR_HFT_2022232.Models;
+
+namespace VDEHYR_HFT_2022232.Repository.Database
+{
+    public class DogsDbContext : DbContext
+    {
+        public DbSet<Dog> Dogs { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Breed> Breeds { get; set; }
+        public DogsDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseLazyLoadingProxies()
+                    .UseInMemoryDatabase("Dogs");
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Breed>(breed => breed
+                .HasOne(breed => breed.Dog)
+                .WithOne(dog => dog.Breed)
+                .OnDelete(DeleteBehavior.Cascade));
+
+            modelBuilder.Entity<Owner>(owner => owner
+                .HasMany(owner => owner.Dogs)
+                .WithOne(dog => dog.Owner)
+                .OnDelete(DeleteBehavior.Cascade));
+
+            //modelBuilder.Entity<Dog>(dog => dog
+            //    .HasOne(dog => dog.Owner)
+            //    .WithOne(owner => owner.Dogs))
+        }
+    }
+}
