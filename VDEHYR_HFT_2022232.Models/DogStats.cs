07e53b5cd510f68bba6ad41d41fa6ce48cfd698e
gaalbence15 <gaalbence15@gmail.com>
2023-05-16 16:51:21 +0200

    Creates DogStats in Model

---
+++ VDEHYR_HFT_2022232.Models/DogStats.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VDEHYR_HFT_2022232.Models
+{
+    public class DogStats
+    {
+        public Owner Owner { get; }
+        public IEnumerable<Dog> Dogs { get; }
+        public DogStats(Owner owner, IEnumerable<Dog> dogs)
+        {
+            Owner = owner;
+            Dogs = dogs;
+        }
+        public override string ToString()
+        {
+            string output = "";
+            output+= Owner.ToString() + "\n";
+            foreach (var dog in Dogs)
+            {
+                output += dog.ToString() + "\n";
+            }
+            return output;
+        }
+    }
+}
