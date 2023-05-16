d37c9869ee35d66081afbdecfd1471b8505c8917
gaalbence15 <gaalbence15@gmail.com>
2023-05-16 18:32:54 +0200

    Adds RestService

--- VDEHYR_HFT_2022232.Client/Program.cs
+++ VDEHYR_HFT_2022232.Client/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Linq;
-using VDEHYR_HFT_2022232.Repository.Database;
-using VDEHYR_HFT_2022232.Repository.Repositories;
 
 namespace VDEHYR_HFT_2022232.Client
 {
@@ -9,15 +7,7 @@ namespace VDEHYR_HFT_2022232.Client
     {
         static void Main(string[] args)
         {
-            DogsDbContext ctx = new DogsDbContext();
-            var dogs = ctx.Dogs.ToArray();
-            var owners = ctx.Owners.ToArray();
-            var breeds = ctx.Breeds.ToArray();
-
-            var dogrepo = new DogRepository(ctx);
-            var dogsread = dogrepo.ReadAll();
-            var dogone = dogrepo.Read(1);
-            ;
+            
         }
     }
 }
