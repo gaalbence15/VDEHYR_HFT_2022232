349af50ccdd7fa3b52ee2d9b5cde4a2344b80c10
gaalbence15 <gaalbence15@gmail.com>
2023-05-22 16:38:02 +0200

    Fixes Dog Controller

--- VDEHYR_HFT_2022232.Endpoint/Controllers/DogLogicController.cs
+++ VDEHYR_HFT_2022232.Endpoint/Controllers/DogLogicController.cs
@@ -1,54 +1,49 @@
-﻿namespace VDEHYR_HFT_2022232.Endpoint.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using VDEHYR_HFT_2022232.Logic.Interfaces;
+using VDEHYR_HFT_2022232.Models;
+
+namespace VDEHYR_HFT_2022232.Endpoint.Controllers
 {
-    using global::VDEHYR_HFT_2022232.Logic.Interfaces;
-    using global::VDEHYR_HFT_2022232.Models;
-    using Microsoft.AspNetCore.Mvc;
-    using System.Collections.Generic;
-    using VDEHYR_HFT_2022232.Logic.Interfaces;
-    using VDEHYR_HFT_2022232.Models;
-
-    namespace VDEHYR_HFT_2022232.Endpoint.Controllers
+    [Route("[controller]")]
+    [ApiController]
+    public class DogLogicController : ControllerBase
     {
-        [Route("[controller]")]
-        [ApiController]
-        public class DogLogicController : ControllerBase
+        IDogLogic logic;
+
+        public DogLogicController(IDogLogic logic)
         {
-            IDogLogic logic;
+            this.logic = logic;
+        }
 
-            public DogLogicController(IDogLogic logic)
-            {
-                this.logic = logic;
-            }
+        [HttpGet]
+        public IEnumerable<Dog> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
 
-            [HttpGet]
-            public IEnumerable<Dog> ReadAll()
-            {
-                return this.logic.ReadAll();
-            }
+        [HttpGet("{id}")]
+        public Dog Read(int id)
+        {
+            return this.logic.Read(id);
+        }
 
-            [HttpGet("{id}")]
-            public Dog Read(int id)
-            {
-                return this.logic.Read(id);
-            }
+        [HttpPost]
+        public void Create([FromBody] Dog value)
+        {
+            this.logic.Create(value);
+        }
 
-            [HttpPost]
-            public void Create([FromBody] Dog value)
-            {
-                this.logic.Create(value);
-            }
+        [HttpPut]
+        public void Update([FromBody] Dog value)
+        {
+            this.logic.Update(value);
+        }
 
-            [HttpPut]
-            public void Update([FromBody] Dog value)
-            {
-                this.logic.Update(value);
-            }
-
-            [HttpDelete("{id}")]
-            public void Delete(int id)
-            {
-                this.logic.Delete(id);
-            }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
         }
     }
 }
