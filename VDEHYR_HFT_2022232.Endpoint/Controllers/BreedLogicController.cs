2f5039f1caab8e14d5450713a883a68d9223517b
gaalbence15 <gaalbence15@gmail.com>
2023-05-22 16:33:17 +0200

    Adds Breed Controller

--- VDEHYR_HFT_2022232.Endpoint/Controllers/BreedLogicController.cs
+++ VDEHYR_HFT_2022232.Endpoint/Controllers/BreedLogicController.cs
@@ -1,6 +1,49 @@
-﻿namespace VDEHYR_HFT_2022232.Endpoint.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using VDEHYR_HFT_2022232.Logic.Interfaces;
+using VDEHYR_HFT_2022232.Models;
+
+namespace VDEHYR_HFT_2022232.Endpoint.Controllers
 {
-    public class BreedLogicController
+    [Route("[controller]")]
+    [ApiController]
+    public class BreedLogicController : ControllerBase
     {
+        IBreedLogic logic;
+
+        public BreedLogicController(IBreedLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Breed> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Breed Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Breed value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Update([FromBody] Breed value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
     }
 }
