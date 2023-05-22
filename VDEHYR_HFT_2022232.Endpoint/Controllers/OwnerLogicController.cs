1270f266987f25439b082e545b4e921563473aac
gaalbence15 <gaalbence15@gmail.com>
2023-05-22 16:35:28 +0200

    Adds Owner Controller

--- VDEHYR_HFT_2022232.Endpoint/Controllers/OwnerLogicController.cs
+++ VDEHYR_HFT_2022232.Endpoint/Controllers/OwnerLogicController.cs
@@ -1,6 +1,49 @@
-﻿namespace VDEHYR_HFT_2022232.Endpoint.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using VDEHYR_HFT_2022232.Logic.Interfaces;
+using VDEHYR_HFT_2022232.Models;
+
+namespace VDEHYR_HFT_2022232.Endpoint.Controllers
 {
-    public class OwnerLogicController
+    [Route("[controller]")]
+    [ApiController]
+    public class OwnerLogicController : ControllerBase
     {
+        IOwnerLogic logic;
+
+        public OwnerLogicController(IOwnerLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Owner> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Owner Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Owner value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Update([FromBody] Owner value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
     }
 }
